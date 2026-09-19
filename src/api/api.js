4ccf6b6e3ca8b6e669c98219f1054edc250df37c
Gargallo-Honesto <rafaelgargallo018@gmail.com>
2026-09-19 21:42:55 +0800

    hayst

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,16 @@
 import axios from "axios";
+import authStore from "../stores/authStore";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-console.log("API URL:", import.meta.env.VITE_API_URL);
+api.interceptors.request.use((config) => {
+  const token = authStore.getState().token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
